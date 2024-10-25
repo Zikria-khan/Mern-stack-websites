6bb7ed85f47a908a4a8b662e68bf4cd94bad0f4c
Practice <coding4443@gmail.com>
2024-10-25 13:46:52 -0700

    change dist

--- Voice-Recorder/backend/server.js
+++ Voice-Recorder/backend/server.js
@@ -10,14 +10,20 @@ require('dotenv').config();
 const app = express();
 
 // CORS Configuration
-app.use(cors({
-  origin: '*', // Allow all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-  credentials: true // Set this to true if you need to allow credentials
-}));
+const allowedOrigins = ['https://apps-three-tan.vercel.app', 'https://another-origin.com'];
 
-app.options('*', cors()); // Enable preflight requests
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, origin); // Allow the origin
+    } else {
+      callback(new Error('Not allowed by CORS')); // Deny the origin
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.use(express.json());
 
